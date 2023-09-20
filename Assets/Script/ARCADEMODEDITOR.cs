e5a42827ba395a6a1825c77a416ec806446947d0
blacOOil <111561598+blacOOil@users.noreply.github.com>
2023-09-20 14:25:16 +0700

    ADD:ARCADEMODESTARTMENU

---
+++ Assets/Script/ARCADEMODEDITOR.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARCADEMODEDITOR : MonoBehaviour
+{
+    public static bool IsGameStart = false;
+    public GameObject StartMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsGameStart == false)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            StartMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+     public void Pause()
+    {
+       
+        IsGameStart = false;
+    }
+   public void Resume()
+    {
+        IsGameStart = true;
+       
+       
+    }
+}
